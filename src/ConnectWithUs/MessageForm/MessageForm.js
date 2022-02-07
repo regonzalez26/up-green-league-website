8a7a2907df04316227c52fd026f009678408ea8c
RJ Gonzalez <carsimdeliez@gmail.com>
2022-02-07 20:31:17 +0800

    add emailjs service

--- src/ConnectWithUs/MessageForm/MessageForm.js
+++ src/ConnectWithUs/MessageForm/MessageForm.js
@@ -1,28 +1,54 @@
 import React from 'react'
 import './MessageForm.css'
 
+import apiKey from './../../emailkey.js'
+import emailjs from '@emailjs/browser'
+emailjs.init(apiKey.USER_ID)
+
 class MessageForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+           .then(
+            (result) => {
+              alert("Message Sent");
+            },
+            (error) => {
+              alert("An error occurred");
+            }
+           );
+  }
+
   render (){
     return(
       <div id="message_form_container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <table>
             <tbody>
               <tr>
                 <td className="message_form_label">Name</td>
-                <td className="message_form_input"><input type="text" /></td>
+                <td className="message_form_input"><input type="text" name="Name"/></td>
               </tr>
               <tr>
                 <td className="message_form_label">Email</td>
-                <td className="message_form_input"><input type="text" /></td>
+                <td className="message_form_input"><input type="text" name="Email"/></td>
               </tr>
               <tr>
                 <td className="message_form_label">Organization</td>
-                <td className="message_form_input"><input type="text" /></td>
+                <td className="message_form_input"><input type="text" name="Organization"/></td>
               </tr>
               <tr>
                 <td className="message_form_label">Message</td>
-                <td className="message_form_input"><textarea rows="10" wrap="hard"></textarea></td>
+                <td className="message_form_input"><textarea type="text" name="Message" rows="10" wrap="hard"></textarea></td>
+              </tr>
+              <tr>
+                <td className="message_form_submit"><input type="submit" value="Send"/></td>
               </tr>
             </tbody>
           </table>
